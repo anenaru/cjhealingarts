211df47d23fba7130e8920e12fc74ddbafab7004
anenaru <theryshan@gmail.com>
2026-07-15 17:01:47 -0400

    add Our Community page with Sanity photo gallery

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'a67qw9j5',
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
   },
+  deployment: {
+    appId: 'ndapcrio0s844yebdyb4pjqx',
+  },
 })
